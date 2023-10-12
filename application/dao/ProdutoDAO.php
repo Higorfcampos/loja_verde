78c3f22f8299e94c98a2a64304405f357066755f
higor.campos <higor.fcampos2001@gmail.com>
2023-10-11 21:40:36 -0400

    Criação de DAO e Construtor

---
+++ application/dao/ProdutoDAO.php
@@ -0,0 +1,20 @@
+<?php
+
+    class ProdutoDAO{
+
+        //CREATE (C)
+        public function salvar($produto){}
+        public function pegatodos(){}
+        //RETRIEVE (R)
+        public function pegaPorId($id){}
+        //UPDATE (U)
+        public function atualizar($preoduto){}
+        //DELETE (D)
+        public function apagar($id){}
+
+    }
+
+
+
+
+?>
